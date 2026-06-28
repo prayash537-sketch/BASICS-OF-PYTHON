dd9752fd3356f77e7c8cb092c7398d0b207caca1
PRAYASH <prayashadhikari537@gmail.com>
2026-06-28 18:41:43 +0545

    Basic question of OOP was solved

--- OOP.py
+++ OOP.py
@@ -1,58 +1,81 @@
 # CLASS
-class Student:
-    college_name = "ABC COLLEGE" #class attribute
-    def __init__(self,fullname,marks):
-        self.name = fullname #object attribute
-        self.marks = marks
+# class Student:
+#     college_name = "ABC COLLEGE" #class attribute
+#     def __init__(self,fullname,marks):
+#         self.name = fullname #object attribute
+#         self.marks = marks
         
     # METHOD
-    def welcome(self):
-        print("Welcome student",self.name)
+    # def welcome(self):
+    #     print("Welcome student",self.name)
         
-    def get_marks(self):
-        return self.marks
+    # def get_marks(self):
+    #     return self.marks
        
 # OBJECT
-s1 = Student('Prayash', 98)
-s1.welcome()
-print(s1.get_marks())
+# s1 = Student('Prayash', 98)
+# s1.welcome()
+# print(s1.get_marks())
 
 # QUESTION
-class Student:
-    def __init__(self,name,marks):
-        self.name = name
-        self.marks = marks
+# class Student:
+#     def __init__(self,name,marks):
+#         self.name = name
+#         self.marks = marks
     
-    @staticmethod
-    def hello():
-        print("Hello")
+#     @staticmethod
+#     def hello():
+#         print("Hello")
     
-    def get_avg(self):
-        sum = 0
-        for val in self.marks:
-            sum += val
-        print("Hello",self.name,"your avg marks in three subject is",sum/3)
+#     def get_avg(self):
+#         sum = 0
+#         for val in self.marks:
+#             sum += val
+#         print("Hello",self.name,"your avg marks in three subject is",sum/3)
     
-s1 = Student('Prayash',[98,95,96])
-s1.get_avg()
-s1.hello()
+# s1 = Student('Prayash',[98,95,96])
+# s1.get_avg()
+# s1.hello()
 
 # ABSTRACTION
-class Car:
-    def __init__(self):
-        self.acc = False
-        self.brk = False
-        self.clutch = False
+# class Car:
+#     def __init__(self):
+#         self.acc = False
+#         self.brk = False
+#         self.clutch = False
     
-    def start(self):
-        self.clutch = True
-        self.acc = True
-        print("Car started..")
+#     def start(self):
+#         self.clutch = True
+#         self.acc = True
+#         print("Car started..")
         
-    def stop(self):
-        self.brk = True
-        print("Car stopped..")
+#     def stop(self):
+#         self.brk = True
+#         print("Car stopped..")
         
-Car1 = Car()
-Car1.start()
-Car1.stop()
+# Car1 = Car()
+# Car1.start()
+# Car1.stop()
+
+#QUESTION
+class Account:
+    def __init__(self,Account,Balance):
+        self.Account = Account
+        self.Balance = Balance
+        
+    def debit(self,amount):
+        self.Balance -= amount
+        print("Rs. ", amount, "was debited")
+        print("Total Balance =",self.get_balance())
+        
+    def credit(self,amount):
+        self.Balance += amount
+        print("Rs. ", amount, "was credited")
+        print("Total Balance =",self.get_balance())
+        
+    def get_balance(self):
+        return self.Balance
+
+acc1 = Account(12345, 10000)
+acc1.debit(1000)
+acc1.credit(500)
